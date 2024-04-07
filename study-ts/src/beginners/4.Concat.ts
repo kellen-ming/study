57919173f7154ee67432b3fc67f4b7ab2417c8ff
梁志明 <liangzhiming@yhmsi.com>
2024-04-07 17:18:56 +0800

    测试一下

---
+++ study-ts/src/beginners/4.Concat.ts
@@ -0,0 +1,20 @@
+
+
+import type { Equal, Expect } from 'type-testing'
+
+
+export type Concat<T extends readonly any[], U extends readonly any[]> = [...T, ...U]
+
+
+const tuple = [1] as const
+
+type cases = [
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
+  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+]
+
+// @ts-expect-error
+type error = Concat<null, undefined>
